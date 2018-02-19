90668a12519b1396db010984063bf86365c465ce
Darko-Martinovic <darko.martinovic@outlook.com>
2018-02-19 15:24:41 +0100

    Init commit

---
+++ TransposingMatrix/Setup/CleanUp.sql
@@ -0,0 +1,41 @@
+﻿--Drop the stored procedure
+IF EXISTS
+(
+    SELECT *
+    FROM sys.objects
+    WHERE object_id = OBJECT_ID(N'[MATRIX].[Transposing]')
+          AND type = 'P'
+)
+    BEGIN
+        DROP PROCEDURE [MATRIX].[Transposing];
+END;
+--Drop helper function 
+IF EXISTS
+(
+    SELECT *
+    FROM sys.objects
+    WHERE object_id = OBJECT_ID(N'[Matrix].[Help]')
+          AND type IN(N'FN', N'IF', N'TF', N'FS', N'FT')
+)
+    DROP FUNCTION [MATRIX].[Help];
+--Drop schema
+IF EXISTS
+(
+    SELECT *
+    FROM sys.schemas
+    WHERE name = N'MATRIX'
+)
+    BEGIN
+        EXEC ('DROP SCHEMA [MATRIX] ');
+END;
+GO
+IF EXISTS
+(
+    SELECT *
+    FROM sys.assemblies
+    WHERE name = N'SimpleTalk.SQLCLR.Matrix'
+)
+    BEGIN
+        DROP ASSEMBLY [SimpleTalk.SQLCLR.Matrix];
+END;
+GO
