27c1ba0bad68dafa6ac2aace2483d22f90030939
Darko-Martinovic <darko.martinovic@outlook.com>
2018-02-20 09:42:18 +0100

    Init commit

--- TransposingMatrix/Database/PreDeployment.sql
+++ TransposingMatrix/Database/PreDeployment.sql
@@ -1,4 +1,5 @@
-﻿/*
+﻿
+/*
  Pre-Deployment Script Template							
 --------------------------------------------------------------------------------------
  This file contains SQL statements that will be executed before the build script.	
@@ -10,52 +11,57 @@
 --------------------------------------------------------------------------------------
 */
 
-
 --------------------------------------------------------------------------------------
 --Create schema if not exists
 --------------------------------------------------------------------------------------
-IF NOT EXISTS (
-	SELECT schema_name
-	FROM information_schema.schemata
-	WHERE schema_name = 'MATRIX' )
-
-BEGIN
-	EXEC sp_executesql N'CREATE SCHEMA MATRIX'
-END
-
+IF NOT EXISTS
+(
+    SELECT schema_name
+    FROM information_schema.schemata
+    WHERE schema_name = 'MATRIX'
+)
+    BEGIN
+        EXEC sp_executesql
+             N'CREATE SCHEMA MATRIX';
+    END;
+---------------------------------------------------------------------------------------
+--- Only for SQL Server 2017+
+-----------------------------------------------
+IF SERVERPROPERTY('productversion') >= '14'
+    BEGIN
 --------------------------------------------------------------------------------------
 --Create asymetric key 
 --
 --Replace the path 'D:\VS2017_PROJECTS\TransposingMatrix\TransposingMatrix\' with your path
 --
 --------------------------------------------------------------------------------------
-IF
+        IF
 (
     SELECT COUNT(*)
     FROM master.sys.asymmetric_keys
     WHERE name LIKE 'askTransposingMatrix%'
 ) = 0
-    BEGIN
-        EXEC sp_executesql
+            BEGIN
+                EXEC sp_executesql
 N'USE MASTER;
 CREATE ASYMMETRIC KEY [askTransposingMatrix]
 FROM FILE = ''D:\VS2017_PROJECTS\TransposingMatrix\TransposingMatrix\askTransposingMatrix.snk''
 ENCRYPTION BY PASSWORD = ''SimpleTalk''';
-    END;
-
-IF NOT EXISTS
+            END;
+        IF NOT EXISTS
 (
     SELECT loginname
     FROM master.dbo.syslogins
     WHERE name = 'loginTransposingMatrix'
 )
-    BEGIN
-        DECLARE @sqlStatement AS NVARCHAR(1000);
-        SELECT @SqlStatement = 'CREATE LOGIN [loginTransposingMatrix] 
+            BEGIN
+                DECLARE @sqlStatement AS NVARCHAR(1000);
+                SELECT @SqlStatement = 'CREATE LOGIN [loginTransposingMatrix] 
 FROM ASYMMETRIC KEY askTransposingMatrix';
-        EXEC sp_executesql
-             @SqlStatement;
-        EXEC sp_executesql
+                EXEC sp_executesql
+                     @SqlStatement;
+                EXEC sp_executesql
 N'USE MASTER;
 GRANT UNSAFE ASSEMBLY TO [loginTransposingMatrix];';
+            END;
     END;
