f157332fd0d3c14042cda354c462cf0aabda4a4d
Darko-Martinovic <darko.martinovic@outlook.com>
2018-02-28 15:24:36 +0100

    Init

---
+++ TransposingMatrix/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with the SQLCLR assembly.
+[assembly: AssemblyTitle("TransposingMatrix")]
+[assembly: AssemblyDescription("An Easier Way of Transposing Query Result in SQL Server")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Darko Martinovic")]
+[assembly: AssemblyProduct("TransposingMatrix")]
+[assembly: AssemblyCopyright("Darko Martinovic")]
+[assembly: AssemblyTrademark("Darko Martinovic")]
+[assembly: AssemblyCulture("")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
