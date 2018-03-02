f9da9cca7ebe76aea005b928e749d0a4b77abe33
Darko-Martinovic <darko.martinovic@outlook.com>
2018-03-02 10:15:54 +0100

    I was added some comments

--- TransposingMatrix/Functions/MatrixHelp.cs
+++ TransposingMatrix/Functions/MatrixHelp.cs
@@ -1,6 +1,4 @@
 using System;
-using System.Data;
-using System.Data.SqlClient;
 using System.Data.SqlTypes;
 using Microsoft.SqlServer.Server;
 using System.Collections;
@@ -11,10 +9,12 @@ namespace TransposingMatrix
 
     public partial class UserDefinedFunctions
     {
-        [Microsoft.SqlServer.Server.SqlFunction(Name = "Help",
-        FillRowMethodName = "ConfigTable_FillRow",
+        [Microsoft.SqlServer.Server.SqlFunction(
+         Name = "Help",                                           // The function name
+        FillRowMethodName = "ConfigTable_FillRow",                
         DataAccess = DataAccessKind.None,
         IsDeterministic = true,
+                                                                  // The output table definition
         TableDefinition = @"ParametarName nvarchar(100), ParametarDescription_______________________________________________________________________________________________________ nvarchar(max)")]
         public static IEnumerable MatrixHelp(
             SqlString ProcedureName  //
@@ -25,37 +25,54 @@ namespace TransposingMatrix
             return list;
 
         }
+
+        //
+        // Insert row
+        //
         private static void ConfigTable_FillRow(object obj, out SqlString ParametarName, out SqlString ParametarDescription)
         {
-            ParametarName = ((System.Collections.Generic.KeyValuePair<string, string>)(obj)).Key;
-            ParametarDescription = ((System.Collections.Generic.KeyValuePair<string, string>)(obj)).Value;
+            ParametarName = ((KeyValuePair<string, string>)(obj)).Key;
+            ParametarDescription = ((KeyValuePair<string, string>)(obj)).Value;
         }
 
+
+        //
+        // Let's describe our parameters
+        //
         public static Dictionary<string, string> GetList(string ProcedureName)
         {
             Dictionary<string, string> data = new Dictionary<string, string>();
             if (ProcedureName.ToLower().Equals("matrix.transposing"))
             {
 
-                data.Add("@Query", "Query or stored procedure, which result will be transposed. Calling a stored procedure always should begin with keyword EXEC.");
+                data.Add("@Query",
+                    "Query or stored procedure, which result will be transposed. Calling a stored procedure always should begin with keyword EXEC.");
 
-                data.Add("@Params", "Query or stored procedure parameters.");
+                data.Add("@Params",
+                    "Query or stored procedure parameters.");
 
-                data.Add("@Rco", "Rotate column ordinal. Default is 1.");
+                data.Add("@Rco",
+                    "Rotate column ordinal. Default is 0.");
 
-                data.Add("@KeyValueOption", "Do we use key-value option. Default is 0.");
+                data.Add("@KeyValueOption",
+                    "Do we use key-value option. Default is 0.");
 
-                data.Add("@ColumnMapping", "Columns mappings.Default is null.");
+                data.Add("@ColumnMapping",
+                    "Columns mappings. Default is null.");
 
-                data.Add("@TableName", "If you like to save result into temporary or permanent table");
+                data.Add("@TableName",
+                    "If you like to save result into temporary or permanent table. For SQL Server 2008+");
 
 
             }
             else
             {
-                data.Add("Error", "Procedure or function " + ProcedureName + " does not exists!");
+                data.Add("Error", "The procedure or the function " + ProcedureName + " does not exists!");
             }
 
+            //
+            // Do memory cleanup
+            //
             for (int i = 0; i <= GC.MaxGeneration; i++)
             {
 #if NET_4_5
