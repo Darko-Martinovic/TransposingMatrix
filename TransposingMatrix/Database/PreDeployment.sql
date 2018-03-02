e0a6f9a9e26aee02064c659df5f53f27633dabf3
Darko-Martinovic <darko.martinovic@outlook.com>
2018-03-02 08:58:42 +0100

    I was added some comments

--- TransposingMatrix/Database/PreDeployment.sql
+++ TransposingMatrix/Database/PreDeployment.sql
@@ -15,40 +15,38 @@ IF NOT EXISTS
 --- Only for SQL Server 2017+
 -----------------------------------------------
 IF SERVERPROPERTY('productversion') >= '14'
-    BEGIN
+BEGIN
 --------------------------------------------------------------------------------------
 --Create asymetric key 
 --
 --Replace the path 'D:\VS2017_PROJECTS\TransposingMatrix\TransposingMatrix\' with your path
 --
 --------------------------------------------------------------------------------------
-        IF
-(
-    SELECT COUNT(*)
-    FROM master.sys.asymmetric_keys
-    WHERE name LIKE 'askTransposingMatrix%'
-) = 0
-            BEGIN
-                EXEC sp_executesql
-N'USE MASTER;
-CREATE ASYMMETRIC KEY [askTransposingMatrix]
-FROM FILE = ''D:\VS2017_PROJECTS\TransposingMatrix\TransposingMatrix\askTransposingMatrix.snk''
-ENCRYPTION BY PASSWORD = ''SimpleTalk''';
-            END;
-        IF NOT EXISTS
-(
-    SELECT loginname
-    FROM master.dbo.syslogins
-    WHERE name = 'loginTransposingMatrix'
-)
-            BEGIN
-                DECLARE @sqlStatement AS NVARCHAR(1000);
-                SELECT @SqlStatement = 'CREATE LOGIN [loginTransposingMatrix] 
-FROM ASYMMETRIC KEY askTransposingMatrix';
-                EXEC sp_executesql
-                     @SqlStatement;
-                EXEC sp_executesql
-N'USE MASTER;
-GRANT UNSAFE ASSEMBLY TO [loginTransposingMatrix];';
-            END;
+    IF
+    (
+	   SELECT COUNT(*)
+	   FROM master.sys.asymmetric_keys
+	   WHERE name LIKE 'askTransposingMatrix%'
+    ) = 0
+    BEGIN
+       EXEC sp_executesql
+				    N'USE MASTER;
+				    CREATE ASYMMETRIC KEY [askTransposingMatrix]
+				    FROM FILE = ''D:\VS2017_PROJECTS\TransposingMatrix\TransposingMatrix\askTransposingMatrix.snk''
+				    ENCRYPTION BY PASSWORD = ''SimpleTalk''';
     END;
+    IF NOT EXISTS
+    (
+        SELECT loginname
+	   FROM master.dbo.syslogins
+	   WHERE name = 'loginTransposingMatrix'
+    )
+    BEGIN
+	   DECLARE @sqlStatement AS NVARCHAR(1000);
+	   SELECT @SqlStatement = 'CREATE LOGIN [loginTransposingMatrix] FROM ASYMMETRIC KEY askTransposingMatrix';
+	   EXEC sp_executesql  @SqlStatement;
+	   EXEC sp_executesql
+                      N'USE MASTER;
+                      GRANT UNSAFE ASSEMBLY TO [loginTransposingMatrix];';
+    END;
+END;
