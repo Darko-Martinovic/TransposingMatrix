b7059b3946377e06f94f4e651e584a063f628b48
Darko-Martinovic <darko.martinovic@outlook.com>
2018-03-02 08:53:49 +0100

    I was added comments to pre-deployment and post-deployment scripts

--- TransposingMatrix/Database/PostDeployment.sql
+++ TransposingMatrix/Database/PostDeployment.sql
@@ -1,15 +1,6 @@
-﻿/*
-Post-Deployment Script Template							
---------------------------------------------------------------------------------------
- This file contains SQL statements that will be appended to the build script.		
- Use SQLCMD syntax to include a file in the post-deployment script.			
- Example:      :r .\myfile.sql								
- Use SQLCMD syntax to reference a variable in the post-deployment script.		
- Example:      :setvar TableName MyTable							
-               SELECT * FROM [$(TableName)]					
---------------------------------------------------------------------------------------
-*/
-
+﻿-------------------------------------------------------------------------
+-- Make the stored procedure parameters default
+-------------------------------------------------------------------------
 ALTER PROCEDURE [dbo].[Transposing]
 	@Query [nvarchar](max),
 	@Params [nvarchar](4000)=NULL,
@@ -27,7 +18,7 @@ IF EXISTS ( SELECT *
             WHERE  id = object_id(N'[MATRIX].[Transposing]') 
                    and OBJECTPROPERTY(id, N'IsProcedure') = 1 )
 BEGIN
-    DROP PROCEDURE [MATRIX].[Transposing]
+    DROP PROCEDURE [MATRIX].[Transposing];
 END
 
 ALTER SCHEMA [MATRIX] TRANSFER [dbo].[Transposing];
@@ -40,7 +31,7 @@ IF EXISTS ( SELECT *
 			WHERE  id = object_id(N'[MATRIX].[Help]') 
 				   and type = N'FT' )
 BEGIN
-	DROP FUNCTION [MATRIX].[Help]
+	DROP FUNCTION [MATRIX].[Help];
 END
 
 ALTER SCHEMA  [MATRIX] TRANSFER dbo.Help;
